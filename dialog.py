3bbd6723c5471d3c776317bfeb54d0f14d85b106
Naman1998 <naman.srivastava1998@gmail.com>
2018-06-14 16:23:41 +0530

    Added Dialog File

--- dialog.py
+++ dialog.py
@@ -1,71 +1,114 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'dialog.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
 
-class Dialog(object):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(431, 258)
 
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 275)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(100, 60, 151, 16))
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(20, 40, 181, 16))
         self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(100, 110, 161, 16))
+
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(30, 90, 55, 16))
         self.label_2.setObjectName("label_2")
-        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit.setGeometry(QtCore.QRect(280, 50, 401, 31))
-        self.textEdit.setObjectName("textEdit")
-        self.textEdit_3 = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit_3.setGeometry(QtCore.QRect(280, 100, 401, 31))
-        self.textEdit_3.setObjectName("textEdit_3")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(340, 180, 93, 28))
+
+        self.label_3 = QtWidgets.QLabel(Dialog)
+        self.label_3.setGeometry(QtCore.QRect(30, 150, 55, 16))
+        self.label_3.setObjectName("label_3")
+
+        self.lineEdit_lat = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_lat.setGeometry(QtCore.QRect(120, 90, 113, 22))
+        self.lineEdit_lat.setObjectName("lineEdit")
+
+        self.lineEdit_long = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_long.setGeometry(QtCore.QRect(120, 150, 113, 22))
+        self.lineEdit_long.setObjectName("lineEdit_2")
+
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(160, 210, 93, 28))
         self.pushButton.setObjectName("pushButton")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
-        self.menubar.setObjectName("menubar")
-        self.menuFile = QtWidgets.QMenu(self.menubar)
-        self.menuFile.setObjectName("menuFile")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-        self.actionExit = QtWidgets.QAction(MainWindow)
-        self.actionExit.setObjectName("actionExit")
-        self.menuFile.addAction(self.actionExit)
-        self.menubar.addAction(self.menuFile.menuAction())
 
-        self.retranslateUi(MainWindow)
+        self.horizontalLayoutWidget = QtWidgets.QWidget(Dialog)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(260, 80, 160, 41))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
 
-        self.pushButton.clicked.connect(self.btn_next)
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
 
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.radioButton_N = QtWidgets.QRadioButton(self.horizontalLayoutWidget)
+        self.radioButton_N.setObjectName("radioButton_2")
+        self.horizontalLayout.addWidget(self.radioButton_N)
 
-    def btn_next(self):
+        self.radioButton_S = QtWidgets.QRadioButton(self.horizontalLayoutWidget)
+        self.radioButton_S.setObjectName("radioButton")
+        self.horizontalLayout.addWidget(self.radioButton_S)
+
+        self.horizontalLayoutWidget_2 = QtWidgets.QWidget(Dialog)
+        self.horizontalLayoutWidget_2.setGeometry(QtCore.QRect(260, 140, 160, 41))
+        self.horizontalLayoutWidget_2.setObjectName("horizontalLayoutWidget_2")
+
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget_2)
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+
+        self.radioButton_W = QtWidgets.QRadioButton(self.horizontalLayoutWidget_2)
+        self.radioButton_W.setObjectName("radioButton_3")
+        self.horizontalLayout_2.addWidget(self.radioButton_W)
+
+        self.radioButton_E = QtWidgets.QRadioButton(self.horizontalLayoutWidget_2)
+        self.radioButton_E.setObjectName("radioButton_4")
+        self.horizontalLayout_2.addWidget(self.radioButton_E)
+
+        self.retranslateUi(Dialog)
+
+        self.pushButton.clicked.connect(self.button_ok)
+
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def button_ok(self):
         global lat, long
-        lat = self.textEdit.toPlainText()
-        long = self.textEdit_3.toPlainText()
+        if self.radioButton_N.isChecked():
+            lat = self.lineEdit_lat.text() + "N"
+        else:
+            lat = self.lineEdit_lat.text() + "S"
+
+        if self.radioButton_W.isChecked():
+            long = self.lineEdit_long.text() + "W"
+        else:
+            long = self.lineEdit_long.text() + "E"
+
         print(lat, long)
 
-    def retranslateUi(self, MainWindow):
+    def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "CORONA IP"))
-        self.label.setText(_translate("MainWindow", "Enter input image location "))
-        self.label_2.setText(_translate("MainWindow", "Enter output image location"))
-        self.pushButton.setText(_translate("MainWindow", "NEXT"))
-        self.menuFile.setTitle(_translate("MainWindow", "File"))
-        self.actionExit.setText(_translate("MainWindow", "Exit"))
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "Enter lat/long for this point :-"))
+        self.label_2.setText(_translate("Dialog", "Latitude"))
+        self.label_3.setText(_translate("Dialog", "Longitude"))
+        self.pushButton.setText(_translate("Dialog", "OK"))
+        self.radioButton_N.setText(_translate("Dialog", "N"))
+        self.radioButton_S.setText(_translate("Dialog", "S"))
+        self.radioButton_W.setText(_translate("Dialog", "W"))
+        self.radioButton_E.setText(_translate("Dialog", "E"))
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Dialog()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
     sys.exit(app.exec_())
 
